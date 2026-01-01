9c2dc64b862ff3a3bc13b965cbd5c18710cb470a
Kaveh Kouvali <81914120+kaveh-dev@users.noreply.github.com>
2026-01-01 13:25:57 +0330

    Added a new system

--- plus/assets/js/mainPageInteractor.js
+++ plus/assets/js/mainPageInteractor.js
@@ -1,3 +1,48 @@
-async function initConnectionToTelegram() {
+const BASE_URL = `https://api.smetu.ir/miniapp`;
+
+async function auth() {
+
+    const initData = Telegram.WebApp.initData;
     
+
+    $.ajax({
+        url: BASE_URL + "/auth",
+        method: "POST",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: JSON.stringify({
+            initData: initData
+        }),
+        success: function (res) {
+            if(res.error && res.error === "NO_TELEGRAM") {
+                window.location.href = "/component-error-page.html";
+            }
+            localStorage.setItem("token", res.token);
+            alert(`Username : ${res.user.username}\nToken : ${res.token}`);
+        },
+        error: function (xhr) {
+            console.error("Auth failed:", xhr.responseText);
+        }
+    });
 }
+
+$(document).ready(async function () {
+    const token = localStorage.getItem("token");
+
+    if(!token) {
+        auth()
+    } else {
+        $.ajax({
+            type: "GET",
+            url: BASE_URL + "/me",           
+            headers: {
+                Authorization: "Bearer " + token
+            },
+            error(xhr) {
+                if(xhr.status === 401) {
+                    auth()
+                }
+            }
+        });
+    }
+});
